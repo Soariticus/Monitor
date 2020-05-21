564a0035cad2a6f6ac641791409005b4dc53141e
Soariticus <37172801+Soariticus@users.noreply.github.com>
2020-05-21 23:56:46 +0200

    Initial commit

---
+++ MonitorAPI.py
@@ -0,0 +1,178 @@
+import time
+import sqlite3 as sql
+import os
+from inspect import currentframe, getframeinfo
+import xlwt
+import xlrd
+from datetime import date
+import asyncio
+from xlutils.copy import copy as xl_copy
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+conn = sql.connect(path + '/databases/data.db')
+c = conn.cursor()
+
+conn2 = sql.connect(path + '/databases/store.db')
+c2 = conn2.cursor()
+
+timeReq = 900
+
+try:  # In the case the database ever needs to be fully wiped, this will set it up again.
+    c.execute("CREATE TABLE users (userID int, name text, oldTime int, hour integer, messages integer)")
+    print("Database (HOURLY) was wiped, setting up database.")
+    #c.execute(f"INSERT INTO users VALUES (1, 5, 'soar', 0)")
+except sql.OperationalError as e:
+    print("Database (HOURLY) is set, no errors.")
+
+try:
+    c2.execute("CREATE TABLE users (userID int, name text, time int, messages int)")
+    print("Database (LONG TERM) was wiped, setting up database.")
+except sql.OperationalError as e:
+    print("Database (LONG TERM) is set, no errors.")
+
+
+class monitor:
+    global conn, c, self
+    # Format: id (INT), oldTime (INT), name (TEXT), messages (INT)
+
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    def read(self, uid, choice):
+        output = None
+        if choice == 1:
+            c.execute(f"SELECT * FROM users WHERE userID = ?", (uid,))
+            output = c.fetchone()
+        elif choice == 2:
+            c2.execute = c.execute(f"SELECT * FROM users WHERE userID = ?", (uid,))
+            output = c2.fetchone()
+        return output
+
+    def getHour(self):
+        curHour = round(time.time()) % 3600
+        secLeft = 3600 - curHour
+        minLeft = secLeft / 60
+        output = [f"Minutes left until next hour = {round(minLeft)}", secLeft]
+        #print(f"Minutes left until next hour = {minLeft}")
+        return output
+
+    async def timer(self, sec):
+        print("!!!!!!!!!!!!!!!!")
+        print(f"waiting {sec}s once")
+        print("!!!!!!!!!!!!!!!!")
+        await asyncio.sleep(sec)
+        self.excWrite(self)
+        c.execute(f"UPDATE users SET messages = 0")
+
+
+
+    async def timerInf(self):
+        while True:
+            print("!!!!!!!!!!!!!!!!")
+            print("Calling addStoreDb() and waiting 3600s")
+            print("!!!!!!!!!!!!!!!!")
+            await asyncio.sleep(3600)
+            c.execute(f"UPDATE users SET messages = 0")
+            self.excWrite(self)
+
+
+    def checkUser(self, uid, name):
+        try:
+            testID = self.read(self, uid, 1)
+            if testID:
+                self.checkCooldown(self, uid)
+            else:
+                print(f"Couldn't find user {name}, creating new profile.")
+                self.newUser(self, uid, name)
+        except sql.OperationalError as e:
+            frameinfo = getframeinfo(currentframe())
+            exit(f"Error: {e} | {frameinfo.filename} | line: {frameinfo.lineno} ")
+
+    def checkCooldown(self, uid):
+        curTime = round(int(time.time()))
+        userS = self.read(id, uid, 1)
+        if (curTime - timeReq) > userS[2]:
+            messages = int(userS[4]) + 1
+            c.execute(f"UPDATE users SET oldTime = {curTime}, messages = {messages} WHERE userID = {uid}")
+            conn.commit()
+            timePassed = curTime - userS[2]
+            db = self.read(id, uid, 1)
+            print(f"Enough time has passed: {timePassed}s/{timeReq}s")
+            print(f"messages = {db[4]}")
+            print("____________________________________________")
+        else:
+            timePassed = curTime - userS[2]
+            print(f"Not enough time has passed: {timePassed}s/{timeReq}s")
+            print("____________________________________________")
+
+    def newUser(self, uid, name):
+        curTime = int(round(time.time()))
+        values = (uid, name, curTime, date.today(), 1)
+        c.execute(f"INSERT INTO users VALUES (?, ?, ?, ?, ?)", values)
+        conn.commit()
+        print(f"New user created with credentials: {uid} : {curTime}")
+        print("____________________________________________")
+
+    def getChoice(self):
+        choice = input("Would you like to [c]heck a user, create a [n]ew user or [r]ead a user? ").lower()
+
+        if choice == "c":
+            test = int(input("What is the ID of the person you'd like to check? "))
+            monitor.checkUser(self, test)
+            print(f'called checkuser on user: {test}')
+
+        elif choice == "n":
+            userID = int(input("What is the ID of the person you'd like to create? "))
+            monitor.newUser(self, userID)
+
+        elif choice == "r":
+            test = int(input("What is the ID of the person you'd like to read? "))
+            thing = monitor.read(self, test, 1)
+            print("user |{test}| returned: {thing}")
+
+    def getAllUsers(self, choice):
+        output = None
+        if choice == 1:
+            c.execute(f"SELECT * FROM users")
+            output = c.fetchall()
+        elif choice == 2:
+            c2.execute(f"SELECT * FROM users")
+            output = c2.fetchall()
+        return output
+
+    def excWrite(self):
+        items = self.getAllUsers(self, 1)
+
+        hour = round(round(time.time()) / (60 * 60))
+
+        oldSheet = xlrd.open_workbook('output.xls', formatting_info=True)
+        book = xl_copy(oldSheet)
+
+        sheet = book.add_sheet(f"Hour {hour}")
+        num = 1
+        sheet.write(0, 0, "Name")
+        sheet.write(0, 1, "Messages")
+        for x in items:
+            sheet.write(num, 0, x[1])
+            sheet.write(num, 1, x[4])
+            num += 1
+        book.save("output.xls")
+
+    async def startTimer(self):
+        time = self.getHour(self)
+        await self.timer(self, time[1])
+        print("Starting infinite timer")
+        await self.timerInf(self)
+
+    def addStoreDb(self):
+        items = self.getAllUsers(self, 1)
+        num = 0
+        for x in items:
+            values = (x[0], x[1], round(time.time()), x[4])
+            c2.execute(f"INSERT INTO users VALUES (?, ?, ?, ?)", values)
+            num += 1
+        conn2.commit()
+
+
+# monitor.getChoice(self=monitor)
+#monitor.excWrite(self=monitor)
---
+++ monitorBot.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+from MonitorAPI import monitor as m
+
+staff = [199576000844136448, 395066428409118720, 659226316679413793, 413955398387630080, 269511972393844736,
+         132739019007197185, 142088223345213440, 638494850861367296, 429083450322976768, 297061136048848898]
+# Dennis, Golden, Grim, Bio, Darkness, SavuBTW, Sav, rev, Safari
+
+support = [320006306486550528, 472444075342037030, 191137771455250432, 521196999073202182, 270655412662042626,
+           242389105864998913]
+# Array, Berry, Rowin, Stormi, Troop, Sansgay
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("Monitoring...")
+        print("____________________________________________")
+        activity = discord.Activity(name='you...', type=discord.ActivityType.watching)
+        await m.startTimer(self=m)
+        await client.change_presence(activity=activity)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.author.id in staff or message.author.id in support:
+            if message.author.id == 142088223345213440:
+                userID = 132739019007197185
+            else:
+                userID = message.author.id
+            try:
+                print(f"Checking user: {message.author}")
+                m.checkUser(self=m, uid=userID, name=message.author.name)
+            except () as e:
+                print(e)
+
+        if message.author.id == 638494850861367296:
+            if message.content.startswith("mon.output"):
+                try:
+                    m.excWrite(self=m)
+                    await message.channel.send("Writing complete, check server files for output.", delete_after=10)
+                except PermissionError:
+                    await message.channel.send("Cannot output whilst file is open on client. Close it and try again.", delete_after=10)
+
+            if message.content.startswith("mon.time"):
+                timeLeft = m.getHour(self=m)
+                await message.channel.send(timeLeft[0])
+
+
+client = MyClient()
+client.run("NzA2MTM1NjQxMTI1MDI3ODgw.XrMFmA.yTg2ULDakirKhK0_NnKtajGD_wM")
